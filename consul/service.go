1caa20780c477d5a44e8d786db575f0e29785147
Kevin <862860000@qq.com>
2019-05-03 15:45:38 +0800

    Update service.go

--- consul/service.go
+++ consul/service.go
@@ -2,9 +2,15 @@ package consul
 
 import (
 	"context"
+	"log"
 
 	"github.com/nilorg/pkg/consul/health/grpc_health_v1"
 	"google.golang.org/grpc"
+)
+
+var (
+	// debug mode
+	Debug = false
 )
 
 // HealthService 健康检查服务
@@ -14,6 +20,9 @@ type HealthService struct {
 
 // Check 实现健康检查接口，这里直接返回健康状态，这里也可以有更复杂的健康检查策略，比如根据服务器负载来返回
 func (h *HealthService) Check(ctx context.Context, req *grpc_health_v1.HealthCheckRequest) (*grpc_health_v1.HealthCheckResponse, error) {
+	if Debug {
+		log.Printf("%s Service Check...", req.GetService())
+	}
 	status := grpc_health_v1.HealthCheckResponse_SERVING
 	if h.ServiceName != req.Service {
 		status = grpc_health_v1.HealthCheckResponse_NOT_SERVING
