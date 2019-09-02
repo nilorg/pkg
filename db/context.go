0b9d48f093317ad6e48665475b406129a2e6a46f
Kevin <862860000@qq.com>
2019-09-02 18:26:33 +0800

    修改注释

--- db/context.go
+++ db/context.go
@@ -14,7 +14,7 @@ var (
 
 type gormKey struct{}
 
-// FromContext 从上下文中获取微信客户端
+// FromContext 从上下文中获取Gorm
 func FromContext(ctx context.Context) (*gorm.DB, error) {
 	c, ok := ctx.Value(gormKey{}).(*gorm.DB)
 	if !ok {
@@ -23,7 +23,7 @@ func FromContext(ctx context.Context) (*gorm.DB, error) {
 	return c, nil
 }
 
-// NewContext 创建微信客户端上下文
+// NewContext 创建Gorm上下文
 func NewContext(ctx context.Context, gdb *gorm.DB) context.Context {
 	return context.WithValue(ctx, gormKey{}, gdb)
 }
