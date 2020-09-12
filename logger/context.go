f3d5495cdc68ba9c953df7a42055c40f3bc44176
Kevin <862860000@qq.com>
2020-09-12 10:11:44 +0800

    Revert "modify context"
    This reverts commit b2f3d793d6ae8dc5082a12e1348ef2b721a56323.

--- logger/context.go
+++ logger/context.go
@@ -12,12 +12,12 @@ import (
 // WithGinContext ...
 func WithGinContext(ctx *gin.Context) context.Context {
 	parent := ctx.Request.Context()
-	if traceID := ctx.GetHeader("X-Trace-Id"); traceID != "" {
+	if traceID := ctx.GetString("X-Trace-Id"); traceID != "" {
 		parent = sdkLog.NewTraceIDContext(parent, traceID)
 	} else {
 		parent = sdkLog.NewTraceIDContext(parent, uuid.New().String())
 	}
-	if spanID := ctx.GetHeader("X-Span-Id"); spanID != "" {
+	if spanID := ctx.GetString("X-Span-Id"); spanID != "" {
 		spanID = trace.StartSpanID(spanID)
 		parent = sdkLog.NewSpanIDContext(parent, spanID)
 	} else {
