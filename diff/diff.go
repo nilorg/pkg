c37feb66ff9d0d0a6b2a756234cd7b79d2cfef88
Kevin <862860000@qq.com>
2021-08-14 17:43:39 +0800

    modify diff string slice

--- diff/diff.go
+++ diff/diff.go
@@ -45,3 +45,28 @@ func StringSlice(src []string, in []string) (added []string, deleted []string) {
 	}
 	return
 }
+
+// StringSliceV2 ...
+func StringSliceV2(src []string, in []string) (added []string, deleted []string, updated []string) {
+	srcSet := mapset.NewSet()
+	for _, i := range src {
+		srcSet.Add(i)
+	}
+	inSet := mapset.NewSet()
+	for _, i := range in {
+		inSet.Add(i)
+	}
+	deletedInterface := srcSet.Difference(inSet).ToSlice()
+	for _, i := range deletedInterface {
+		deleted = append(deleted, i.(string))
+	}
+	addedInterface := inSet.Difference(srcSet).ToSlice()
+	for _, i := range addedInterface {
+		added = append(added, i.(string))
+	}
+	updatedInterface := srcSet.Intersect(inSet).ToSlice()
+	for _, i := range updatedInterface {
+		updated = append(updated, i.(string))
+	}
+	return
+}
